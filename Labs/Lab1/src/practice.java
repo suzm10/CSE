2ed8dc948a8c0a752531f7094cb95405dcff737a
Suzan M <75396711+suzm10@users.noreply.github.com>
2021-03-04 14:51:19 -0500

    upload lab 1

---
+++ Labs/Lab1/src/practice.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+public class practice {
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("Hello");
+    }
+}
